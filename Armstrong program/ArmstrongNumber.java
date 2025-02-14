050057ba24bd64f37878e8dfb2a86a7fa4b737b8
NikhileshKothmire <155325198+NikhileshKothmire@users.noreply.github.com>
2025-02-14 12:58:43 +0530

    Commet-1
    WAP Armstrong Number Program

---
+++ Armstrong program/ArmstrongNumber.java
@@ -0,0 +1,50 @@
+// 13 Feb 2025 ( Armstrong Number)
+
+class ArmstrongNumber
+{
+
+	public static void main(String[] args)
+	 {
+		int num=135 , lent = 0, sum=0;
+		int dup=num;
+		for(int i=num;i>0;i/=10)
+		{
+			lent++;
+		}
+
+	while(num>0)
+
+	{
+		int rem = num%10;
+		int pow =1;
+		for(int i=1;i<=lent;i++)
+			pow*=rem;
+		sum+=pow;
+		num/=10;
+
+	}
+	System.out.println(dup==sum?dup+ "  is Armstrong Number  " : 
+                       dup+ "  is Not Armstrong Number ");
+	}
+}
+
+
+/*    O/P
+
+E:\Qspider\Java  Programs>javac ArmstrongNumber.java
+
+E:\Qspider\Java  Programs>java ArmstrongNumber
+153is Armstrong Number
+
+E:\Qspider\Java  Programs>
+
+---------------------------------------------------------
+E:\Qspider\Java  Programs>javac ArmstrongNumber.java
+
+E:\Qspider\Java  Programs>java ArmstrongNumber
+135  is Not Armstrong Number
+
+E:\Qspider\Java  Programs>
+
+*/
+
